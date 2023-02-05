52c60aaa5e9d74d86c392feab3bf51df9ca93dcd
giffengood <matthewstyzinski@gmail.com>
2023-02-05 16:07:40 +1100

    init

---
+++ .zshrc
@@ -0,0 +1,98 @@
+# top of your .zshrc file
+
+export ZIT_MODULES_PATH="${ZDOTDIR}/zit"
+export PATH=$HOME/.local/bin:$PATH
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# UNCOMMENT TO ENABLE AUTO NODE VERSION SWITCHING 
+
+# autoload -U add-zsh-hook
+# load-nvmrc() {
+#   local node_version="$(nvm version)"
+#   local nvmrc_path="$(nvm_find_nvmrc)"
+
+#   if [ -n "$nvmrc_path" ]; then
+#     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+#     if [ "$nvmrc_node_version" = "N/A" ]; then
+#       nvm install
+#     elif [ "$nvmrc_node_version" != "$node_version" ]; then
+#       nvm use
+#     fi
+#   elif [ "$node_version" != "$(nvm version default)" ]; then
+#     echo "Reverting to nvm default version"
+#     nvm use default
+#   fi
+# }
+# add-zsh-hook chpwd load-nvmrc
+# load-nvmrc
+
+# Options
+zle_highlight=('paste:none')
+unsetopt BEEP
+
+#############################################################################
+# History Configuration
+##############################################################################
+HISTSIZE=5000               #How many lines of history to keep in memory
+HISTFILE=${ZDOTDIR}/.zsh_history     #Where to save history to disk
+SAVEHIST=5000               #Number of history entries to save to disk
+# HISTDUP=erase               #Erase duplicates in the history file
+unsetopt EXTENDED_HISTORY
+setopt    appendhistory     #Append history to the history file (no overwriting)
+setopt    sharehistory      #Share history across terminals
+setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
+
+# Source Zit file
+source "${ZIT_MODULES_PATH}/zit.zsh"
+
+# Install and loads plugins
+zit-il "https://github.com/hlissner/zsh-autopair" "plugins/zsh-autopair" "autopair.zsh"
+zit-il "https://github.com/zsh-users/zsh-autosuggestions" "plugins/zsh-autosuggestions" "zsh-autosuggestions.zsh"
+zit-il "https://github.com/dashixiong91/zsh-vscode" "plugins/zsh-vscode" "zsh-vscode.plugin.zsh"
+zit-il "https://github.com/zsh-users/zsh-completions" "plugins/zsh-completions" "zsh-completions.plugin.zsh"
+zit-il "https://github.com/hcgraf/zsh-sudo" "plugins/sudo" "sudo.plugin.zsh"
+
+eval "$(zoxide init zsh)"
+
+# ALIAS
+# alias ls="ls -lhaG --color=always | sed -re 's/^[^ ]* //'"
+alias ls="ls -lhaG --color=always"
+# alias la = "!git config -l | grep alias | cut -c 7-"
+alias code="vs"
+alias reload="exec $SHELL"
+
+# BINDS
+
+# Basic auto/tab complete:
+
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+_comp_options+=(globdots) # Include hidden files.
+
+autoload -Uz compinit
+
+for dump in ~/.zcompdump(N.mh+24); do
+  compinit
+done
+
+compinit -C
+
+# Prompt theme
+eval "$(starship init zsh)"
+
+
+# Needs to be at end of file
+zit-il "https://github.com/zsh-users/zsh-syntax-highlighting" "plugins/zsh-syntax-highlighting" "zsh-syntax-highlighting.zsh"
+
+# script to compile ZSH files and speed-up loading
+# should be called last
+zit-lo "/" "compile-zsh-files.zsh"
+
+
+# zprof # bottom of .zshrc
---
+++ zit/zit.zsh
@@ -0,0 +1,127 @@
+# Zit - minimal plugin manager for ZSH
+
+# store all loaded modules to paths
+export -Ua ZIT_MODULES_LOADED
+
+# store all modules to be upgraded
+export -Ua ZIT_MODULES_UPGRADE
+
+# set variable below to change zit modules path
+if [[ -z "${ZIT_MODULES_PATH}" ]]; then
+  export ZIT_MODULES_PATH="${ZDOTDIR}/zit"
+fi
+
+_zit-param-validation() {
+  local name="${1}"
+  local param="${2}"
+
+  if [[ -z "${param}" ]]; then
+    printf '[zit] Missing argument: %s\n' "${name}"
+    return 1
+  fi
+  return 0
+}
+
+# loader
+zit-load() {
+  _zit-param-validation 'Module directory' "${1}" || return 1
+  _zit-param-validation '.zsh file' "${2}" || return 2
+
+  local module_dir="${ZIT_MODULES_PATH}/${1}"
+  local dot_zsh="${2}"
+
+  # shellcheck source=/dev/null
+  source "${module_dir}/${dot_zsh}" || return 255
+  # added to global dir array for tracking
+  ZIT_MODULES_LOADED+=("${module_dir}")
+}
+
+# installer
+zit-install() {
+  _zit-param-validation 'Git repo' "${1}" || return 1
+  _zit-param-validation 'Module directory' "${2}" || return 2
+
+  local git_url="${1}"
+  local module_dir="${ZIT_MODULES_PATH}/${2}"
+
+  # http://zsh.sourceforge.net/Doc/Release/Expansion.html#Parameter-Expansion
+  # https://github.com/thiagokokada/zit#branch -> https://github.com/thiagokokada/zit
+  local git_repo="${git_url%'#'*}"
+
+  local git_branch="${git_url#*'#'}"
+  # https://github.com/thiagokokada/zit -> master
+  # https://github.com/thiagokokada/zit# -> master
+  # https://github.com/thiagokokada/zit#branch -> branch
+  if [[ -z "${git_branch}" ]] || [[ "${git_branch}" == "${git_url}" ]]; then
+    git_branch="master"
+  fi
+
+  # clone module
+  if [[ ! -d "${module_dir}" ]]; then
+    printf 'Installing %s\n' "${module_dir}"
+    git clone --recurse-submodules \
+      "${git_repo}" -b "${git_branch}" "${module_dir}"
+    printf '\n'
+  fi
+  # added to global dir array for updater
+  [[ -z "${ZIT_DISABLE_UPGRADE}" ]] && ZIT_MODULES_UPGRADE+=("${module_dir}")
+}
+
+# do both above in one step
+zit-install-load() {
+  _zit-param-validation 'Git repo' "${1}" || return 1
+  _zit-param-validation 'Module directory' "${2}" || return 2
+  _zit-param-validation '.zsh file' "${3}" || return 3
+
+  local git_repo="${1}"
+  local module_dir="${2}"
+  local dot_zsh="${3}"
+
+  zit-install "${git_repo}" "${module_dir}"
+  zit-load "${module_dir}" "${dot_zsh}"
+}
+
+# upgrade modules
+zit-upgrade() {
+  local module_dir
+
+  for module_dir in "${ZIT_MODULES_UPGRADE[@]}"; do
+    pushd "${module_dir}" > /dev/null || continue
+    printf 'Updating %s\n' "${module_dir}"
+    git pull
+    printf '\n'
+    popd > /dev/null || continue
+  done
+}
+
+# remove module
+zit-remove() {
+  _zit-param-validation 'Module directory' "${1}" || return 1
+
+  local module_dir="${1}"
+  local module_path="${ZIT_MODULES_PATH}/${module_dir}"
+
+  if [[ -z "${ZIT_MODULES_UPGRADE[(r)${module_path}]}" ]]; then
+    printf 'No module %s is installed.\n' "${module_dir}"
+    return 255
+  fi
+
+  while true; do
+    printf 'Really delete module %s? (y/n) ' "${module_dir}"
+    read -r yn
+    case $yn in
+      [Yy]* ) rm -rf "${ZIT_MODULES_PATH:?}/${module_dir}"
+              break;;
+      [Nn]* ) break;;
+      * ) echo 'Please answer (y)es or (n)o.';;
+    esac
+  done
+}
+
+alias zit-in='zit-install'
+alias zit-lo='zit-load'
+alias zit-il='zit-install-load'
+alias zit-up='zit-upgrade'
+alias zit-rm='zit-remove'
+
+# vim: ft=zsh
